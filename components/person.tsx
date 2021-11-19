738176b610b37d29fddd4c6249b2154f62dc0a5c
Maxwell <hex0@live.com>
2021-11-19 22:25:41 +0000

    setup person model

---
+++ components/person.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export interface IPersonProps {
+  nickname: string;
+  fistName: string;
+  lastName: string;
+}
+
+export default function Person({ nickname, fistName, lastName }: IPersonProps) {
+  return (
+    <div>
+      <h1>{nickname}</h1>
+      <p>
+        {fistName} {lastName}
+      </p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
 import type { NextPage } from "next";
+import Person from "../components/person";
+import { v4 as uuidv4 } from "uuid";
+
+interface IPerson {
+  id: string;
+  nickname: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface data {
+  person: IPerson[];
+}
+
+const data = [
+  {
+    id: uuidv4(),
+    nickname: "Juan",
+    firstName: "Juan",
+    lastName: "Perez",
+  },
+];
 
 const Home: NextPage = () => {
-  return <div className="mrk-hello">Hello</div>;
+  return (
+    <div className="mrk-hello">
+      {data.map((person) => (
+        <Person
+          key={person.id}
+          fistName={person.firstName}
+          lastName={person.lastName}
+          nickname={person.nickname}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
