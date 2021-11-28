0e59930d898c4a80daccfbb30b46b32da28deb5f
Maxwell <hex0@live.com>
2021-11-28 22:07:13 +0000

    add person form

--- components/addNewPerson.tsx
+++ components/addNewPerson.tsx
@@ -1,11 +1,44 @@
+import { useFormik } from "formik";
 import * as React from "react";
 
 export interface IAddNewPersonProps {}
 
 export default function AddNewPerson(props: IAddNewPersonProps) {
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      nickname: "",
+      fistName: "",
+      lastName: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
-    <div>
-      <h1>Add New Person</h1>s
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nickname">Nickname</label>
+      <input
+        type="text"
+        name="nickname"
+        onChange={handleChange}
+        value={values.nickname}
+      />
+      <label htmlFor="fistName">First Name</label>
+      <input
+        type="text"
+        name="fistName"
+        onChange={handleChange}
+        value={values.fistName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={values.lastName}
+      />
+
+      <button type="submit">Add Person</button>
+    </form>
   );
 }
