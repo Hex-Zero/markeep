e1bd5dd35f68c9e6446042a4a05f8085215f6f91
Maxwell <hex0@live.com>
2021-11-28 21:38:35 +0000

    add new person component

---
+++ components/addNewPerson.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface IAddNewPersonProps {}
+
+export default function AddNewPerson(props: IAddNewPersonProps) {
+  return (
+    <div>
+      <h1>Add New Person</h1>s
+    </div>
+  );
+}
