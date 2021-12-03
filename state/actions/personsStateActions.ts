c00148e2059e8336daa96890b17aab2a308d3afd
Maxwell <hex0@live.com>
2021-12-03 21:31:43 +0000

    that was not part of the plan

--- state/actions/personsStateActions.ts
+++ state/actions/personsStateActions.ts
@@ -2,5 +2,4 @@ export enum PersonsStateActionTypes {
   GET_PERSONS = "[PersonsState] GET_PERSONS",
   GET_PERSONS_SUCCESS = "[PersonsState] GET_PERSONS_SUCCESS",
   GET_PERSONS_FAILURE = "[PersonsState] GET_PERSONS_FAILURE",
-  GET_PERSON = "[PersonsState] GET_PERSON",
 }
