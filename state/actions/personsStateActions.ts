06e2c0a13d902f1fd81a30c77f0542066f356ec9
Maxwell <hex0@live.com>
2021-12-05 21:43:36 +0000

    ass

--- state/actions/personsStateActions.ts
+++ state/actions/personsStateActions.ts
@@ -2,10 +2,5 @@ export enum PersonsStateActionTypes {
   GET_PERSONS = "[PersonsState] GET_PERSONS",
   GET_PERSONS_SUCCESS = "[PersonsState] GET_PERSONS_SUCCESS",
   GET_PERSONS_FAILURE = "[PersonsState] GET_PERSONS_FAILURE",
-
   GET_PERSON = "[PersonsState] GET_PERSON",
-
-  CREATE_PERSON = "[PersonsState] CREATE_PERSON",
-
-  UPDATE_PERSON = "[PersonsState] UPDATE_PERSON",
 }
