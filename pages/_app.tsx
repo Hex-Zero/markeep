fef91a7dc53b336ce72969cb699799d22b72f6c0
Maxwell <hex0@live.com>
2022-01-04 18:44:50 +0000

    setup session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import "../styles/style.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <title>Markeep</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
