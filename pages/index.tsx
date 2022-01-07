94b55b41d8ad7705cfd3cad0f13a311439674ebd
Maxwell <hex0@live.com>
2022-01-07 23:36:35 +0000

    less and less

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
 
   return (
     <main>
-      {status === "loading" && <div>Loading...</div>}
+      {status === "loading" && <div>Loading..</div>}
       {!data ? (
         <button onClick={() => signOut()}>Sign out</button>
       ) : (
