f1e1ab8eddca74accf6748e44471495220de2ea7
Maxwell <hex0@live.com>
2022-01-27 20:52:52 +0000

    remove unused imports

--- helpers/seedData.ts
+++ helpers/seedData.ts
@@ -2,7 +2,6 @@ import faker from "faker";
 import { IPerson } from "../interfaces/IPerson";
 import { v4 as uuidv4 } from "uuid";
 import { inputType } from "../interfaces/IInputType";
-import { getSeedImage } from "./seedImage";
 
 export const getSeedData = (amount: number = 0) => {
   const seedData: IPerson[] = [];
